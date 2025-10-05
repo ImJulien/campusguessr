3fc7e42eaa7e8e57c0ea30027fe5cdf32f50a0d9
ImJulien <64613853+ImJulien@users.noreply.github.com>
2025-10-05 01:22:47 -0700

    email verify stuff

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth';
+import verifyEmailRoutes from './routes/verify-email'; 
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/email', verifyEmailRoutes); 
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Root works!');
--- server/src/routes/verify-email.ts
+++ server/src/routes/verify-email.ts
@@ -1,15 +1,53 @@
 import express, { Request, Response } from 'express';
 import { PrismaClient } from '../generated/prisma';
+import {Resend} from 'resend';
+import crypto from 'crypto';
 
 const prisma = new PrismaClient();
-const crypto = require('crypto');
 const router = express.Router();
+const resend = new Resend(process.env.RESEND_API_KEY!);
 
-router.post('/verify-email', async (req: Request, res: Response) => {
-    const { email } = req.body;
-    const domain = email.split('@')[1];
+router.post('/send', async (req: Request, res: Response) => {
+    const { userId } = req.body;
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+    if (!user) {
+        return res.status(400).json({ error: 'User not found' });
+    }
 
+    const token = crypto.randomBytes(32).toString('hex');
 
+    await prisma.user.update({
+        where: { id: userId },
+        data: { verificationToken: token }
+    });
+
+    await resend.emails.send({
+        from: 'CampusGuessr <onboarding@resend.dev>',
+        to: user.email,
+        subject: 'Verify your email',
+        html: `<a href="http://localhost:3000/verify?token=${token}">Verify Email</a>`,
+    });
+
+    res.json({ message: 'Email sent' });
+});
+
+router.get('/verify', async (req: Request, res: Response) => {
+    const { token } = req.query;
+
+    const user = await prisma.user.findFirst({
+        where: { verificationToken: token as string }
+    });
+
+    if (!user) {
+        return res.status(400).json({ error: 'Invalid token' });
+    }
+
+    await prisma.user.update({
+        where: { id: user.id },
+        data: { emailVerified: true, verificationToken: null }
+    });
+    res.json({ message: 'Email verified' });
 });
 
 export default router;
+
